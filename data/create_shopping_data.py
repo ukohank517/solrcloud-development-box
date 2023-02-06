ee62c5977d30bde274e3ba738acdc8463d027e82
ukohank517 <ukohank517@gmail.com>
2023-02-06 23:40:30 +0900

    add data as submodule

--- data/create_shopping_data.py
+++ data/create_shopping_data.py
@@ -6,9 +6,12 @@ SPLIT_SIZE=10000
 INPUT_FOLDER='esci-data/shopping_queries_dataset'
 OUTPUT_FOLDER='split-json-data'
 
+
+print("reading data file ...")
 df_examples = pd.read_parquet(f'{INPUT_FOLDER}/shopping_queries_dataset_examples.parquet')
 df_products = pd.read_parquet(f'{INPUT_FOLDER}/shopping_queries_dataset_products.parquet')
 df_sources = pd.read_csv(f"{INPUT_FOLDER}/shopping_queries_dataset_sources.csv")
+print("data reading complete")
 
 df_examples_products = pd.merge(
     df_examples,
@@ -29,4 +32,5 @@ for i in range(0, len(df_examples_products), SPLIT_SIZE):
     data_to = min((i + SPLIT_SIZE), len(df_examples_products)) - 1
     with open(f"{OUTPUT_FOLDER}/shopping_products_{str(i // SPLIT_SIZE).zfill(3)}.json", "w") as outputFile:
         json.dump(json.loads(df_examples_products[data_from:data_to].to_json(orient='records')), outputFile, indent=2)
-    print(i)
+    print(f'\rcreating json file {i//SPLIT_SIZE} / {len(df_examples_products)//SPLIT_SIZE}', end='')
+print(f'\nsuccessfully creating feeding json file in [{OUTPUT_FOLDER}]')
